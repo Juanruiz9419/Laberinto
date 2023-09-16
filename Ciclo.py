007e362393f14d6f4eb0446fff122eb53f39a3a1
Juanruiz <juandavidruiznievas@gmail.com>
2023-09-15 19:38:13 -0500

    Ciclo50 pasa a Ciclo

---
+++ Ciclo.py
@@ -0,0 +1,19 @@
+import os
+import readchar
+
+print(f"preciona la tecla 'N' hasta que te indique parar!!")
+
+
+def limpiar():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+while True:
+    tcl =readchar.readkey()
+    contador = 0
+
+    if tcl <= readchar.key {78}:
+        limpiar
+        contador + 1
+        print(f"numero" , contador)
+    else:
+        print(f"Detente, fin del conteo")    
